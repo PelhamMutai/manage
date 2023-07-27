679bacb5fc0d0cbdd5bca5ca8bfda3e45b738e31
pelham mutai <mutaipelham@gmail.com>
2023-07-27 17:13:44 +0300

    updated nav

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-const navList=document.querySelector(".nav-list");
 const navToggle=document.querySelector(".nav-toggle");
 const prNav=document.querySelector(".navbar");
 const toggIcon=document.querySelectorAll(".icon");
@@ -8,13 +7,12 @@ navToggle.addEventListener("click", () =>{
   prNav.classList.toggle("open");
   toggIcon.forEach(icon =>{
     icon.classList.toggle("close");
-    navList.classList.toggle("data-overlay");
   });
 
 });
 
 const slider = new A11YSlider(document.querySelector('.slider'), {
-  adaptiveHeight: false,
+  adaptiveHeight: false, 
   dots: true,
   arrows:false,
   responsive: {
